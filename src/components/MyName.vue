e30613342fbee85bc58d1c60797d990f1c1fce92
tniu0003 <tniu0003@student.monahs.edu>
2024-07-25 12:13:23 +1000

    first commit

---
+++ src/components/MyName.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="my-name">
+    <h1>Hello, my name is Tianyu Niu</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName'
+}
+</script>
+
+<style scoped>
+.my-name {
+  text-align: center;
+  margin-top: 50px;
+}
+
+h1 {
+  color: #42b983; /* Vue green color */
+  font-family: 'Roboto', sans-serif;
+}
+</style>
